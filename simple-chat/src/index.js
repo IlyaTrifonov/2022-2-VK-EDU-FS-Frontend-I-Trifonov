77919f391f58f8cf25e74b977e4edfe24f603f0f
IlyaTrifonov <ilya.trifonov.official@gmail.com>
2022-10-25 11:54:57 +0300

    Appbar added.

--- simple-chat/src/index.js
+++ simple-chat/src/index.js
@@ -1,1 +1,20 @@
-import './index.css';
+// import './index.css';
+
+
+const form = document.querySelector('form');
+const input = document.querySelector('.form-input');
+const message = document.querySelector('.message');
+
+form.addEventListener('submit', handleSubmit.bind());
+form.addEventListener('keypress', handleKeyPress.bind());
+
+function handleSubmit(event) {
+    event.preventDefault();
+    message.innerText = input.value;
+}
+
+function handleKeyPress(event) {
+    if (event.keyCode === 13) {
+        form.dispatchEvent(new Event('submit'));
+    }
+}
